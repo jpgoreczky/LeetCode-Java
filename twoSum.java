1d16c45aafd95247fb9a14103b91c661c170927c
Josie Goreczky <107211572+jpgoreczky@users.noreply.github.com>
2025-11-12 18:08:21 -0500

    Update problem with multiple solutions

--- twoSum.java
+++ twoSum.java
@@ -6,7 +6,7 @@ class Solution {
         // if the sume of p1 and p2 > target, move p2 left (smaller sum)
         // if the sum of p1 and p2 < target, move p1 right (bigger sum)
 
-        // O(N*N)
+        // O(N*N) - brute force
         int[] ans = new int[2];
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length; j++) {
@@ -25,14 +25,31 @@ class Solution {
 
 // you can create a hashmap to see if required number exists without searching and get the index of 
 // it through value of the element in key - value pair
-// O(1)
+// two-pass O(N) - 2 for loops
+    Map<Integer, Integer> numMap = new HashMap<>();
 
-    //    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        numMap.put(nums[i], i);
+    }
 
-    //    for (int i = 0; i < nums.length; i++) {
-    //         if (map.containsKey(target - nums[i])) {
-    //             return new int[] {i, map.get(target-nums[i])};
-    //         }
-    //         map.put(nums[i], i);
-    //    }
-    //    return new int[] {-1,-1};
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+            return new int[] {i, numMap.get(complement)};
+        }
+    }
+
+    return new int[] {-1,-1}; // no solution
+
+
+// O(N) - one pass
+
+       HashMap<Integer, Integer> map = new HashMap<>();
+
+       for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[] {i, map.get(target-nums[i])};
+            }
+            map.put(nums[i], i);
+       }
+       return new int[] {-1,-1};
