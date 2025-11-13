0a462d4c5ba03251e96907988ed1ff4421a8279a
Josie Goreczky <107211572+jpgoreczky@users.noreply.github.com>
2025-11-12 19:05:34 -0500

    fix reasoning

--- longestSubstringWithoutRepeatingCharacters.java
+++ longestSubstringWithoutRepeatingCharacters.java
@@ -19,9 +19,9 @@ class Solution {
         for (int i = 0; i < len; i++) {
             //  at the start the set is empty
             // if the current char isn't in the set, it adds it and keeps track of the length
-            // if the current char is in the set, it'll remove the original duplicate char from the set
+            // while the current char is in the set, it'll remove the original duplicate char from the set
             // and increment the second pointer to the next index
-            // when the set no longer has the duplicate characters in the set, it'll add the current char
+            // until the set no longer has the duplicate characters in the set, it'll add the current char
             // into the set and the the first pointer increments from the for loop
             if (!charSet.contains(s.charAt(i))) {
                 charSet.add(s.charAt(i));
